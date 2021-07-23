7bff9ab8b92a91c0e4f49959a0251492758dfc16
SorviusN <Opt1206@gmail.com>
2021-07-22 23:38:59 -0700

    READMe.md Added

--- Lab09-LINQ/Program.cs
+++ Lab09-LINQ/Program.cs
@@ -13,13 +13,16 @@ namespace Lab09_LINQ
 		static void Main(string[] args)
 		{
 			int count = 0;
+
 			// taking JObject key of features and putting it into the list.
 			IList<JToken> featureList = ParseJSON()["features"].ToList();
+
 			IEnumerable<string> neighborhoods =
 				from feature in featureList
 				where ((string)feature.SelectToken("properties.neighbordhood") != "")
-				// Cast the selectoken method result to a string, where we are passing in our desired input.
+				// Cast the SelecToken method result to a string, where we are passing in our desired input.
 				select (string)feature.SelectToken("properties.neighborhood");
+
 			foreach (string names in neighborhoods)
 			{
 				Console.WriteLine(names);
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # LINQ Project
 
 ## Summary - 
+This project uses the System.LINQ library in conjunction with NewtonSoft to filter out various
+aspects of our data. In this exercise we attempted to gather specific info from the JSON
+that is filtered using methods.  
+
+## Visual -
+![whiteboard](./Lab09-Visual.PNG)
+
+### Collaborators -
+
+- Dave Arno  
+- Joshua Haddock
+- Steven Boston
+- Benjamin Ibarra
